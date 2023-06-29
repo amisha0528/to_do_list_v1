35fc327126f16a582f058402432152045a9c9ea0
amisha5635 <92245886+amisha5635@users.noreply.github.com>
2023-06-30 00:17:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.get('/work', function (request, response) {
 //     response.redirect("/work");
 // })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT ||3000, function () {
     console.log("listening on port 3000");
 });
